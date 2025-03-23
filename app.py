0c95dba2a96257dee9caf3daef4561fe775a76a6
prabhuji12 <guru.adu05@gmail.com>
2025-03-23 21:27:21 +0530

    updated results output

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, jsonify
 import json
-import os
 
 app = Flask(__name__)
-
 
 def load_json(file_path):
     with open(file_path, 'r') as file:
@@ -11,21 +9,20 @@ def load_json(file_path):
 
 @app.route('/')
 def index():
-    results = load_json('results.json')
-    best_models = load_json('best_models.json')
-    return render_template('index.html', results=results, best_models=best_models)
+    results_no_tuning = load_json('results_no_tuning.json')
+    results_tuned = load_json('results_tuned.json')
+    best_models = load_json('best_hyperparameters.json')
+
+    return render_template(
+        'index.html', 
+        results_no_tuning=results_no_tuning, 
+        results_tuned=results_tuned, 
+        best_models=best_models
+    )
 
 @app.route('/notebook')
 def notebook():
     return render_template('notebook.html')
-
-@app.route('/api/results')
-def api_results():
-    return jsonify(load_json('results.json'))
-
-@app.route('/api/best_models')
-def api_best_models():
-    return jsonify(load_json('best_models.json'))
 
 if __name__ == '__main__':
     app.run(debug=True)
